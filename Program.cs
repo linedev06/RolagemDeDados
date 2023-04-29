07b0fc3cd5f31575294209290404365ce421fe91
linedev06 <vitorafelismino@gmail.com>
2023-04-29 15:17:13 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+int resultado;
+int FacesDoDado;
+
+Console.WriteLine("--- Rolagem de Dados ---\n");
+
+Console.WriteLine("Digite a quantidade das faces do dado: ");
+FacesDoDado = Convert.ToInt32(Console.ReadLine());
+
+Random random = new Random();
+
+resultado = random.Next(1, FacesDoDado + 1);
+
+
+Console.WriteLine($"O resultado da rolagem do {FacesDoDado} é: {resultado}");
